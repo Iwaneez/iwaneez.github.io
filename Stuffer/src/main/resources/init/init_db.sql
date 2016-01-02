ef934ad2edd20bede45573055e5be4f6b5490548
Iwaneez <ivan.harmady@gmail.com>
2016-01-03 00:43:49 +0100

    .

--- Stuffer/src/main/resources/init/init_db.sql
+++ Stuffer/src/main/resources/init/init_db.sql
@@ -11,13 +11,13 @@ CREATE TABLE user_roles (
   CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username));
   
 INSERT INTO users(username,password,enabled)
-VALUES ('Iwaneez','123456', true);
+VALUES ('iwaneez','9b6173e5991a7db6aa91fb2b40e5785c36079987f5eebb68946eb0ea04f3ee7b882cd760366e4262', true);
 INSERT INTO users(username,password,enabled)
-VALUES ('alex','123456', true);
+VALUES ('alex','3ef30b19ce1ddf363ec760ea55bdffb1b943ebf56d9f16034f5bab78ecfc1496456ad6361d017a49', true);
 
 INSERT INTO user_roles (username, role)
-VALUES ('Iwaneez', 'ROLE_USER');
+VALUES ('iwaneez', 'ROLE_USER');
 INSERT INTO user_roles (username, role)
-VALUES ('Iwaneez', 'ROLE_ADMIN');
+VALUES ('iwaneez', 'ROLE_ADMIN');
 INSERT INTO user_roles (username, role)
 VALUES ('alex', 'ROLE_USER');
