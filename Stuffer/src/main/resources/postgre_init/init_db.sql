01ba40090f6a29829178836b0da177c46488be05
Iwaneez <ivan.harmady@gmail.com>
2016-01-03 01:52:49 +0100

    updated init script

--- Stuffer/src/main/resources/postgre_init/init_db.sql
+++ Stuffer/src/main/resources/postgre_init/init_db.sql
@@ -1,7 +1,7 @@
 CREATE  TABLE users (
   username VARCHAR(45) NOT NULL ,
   password VARCHAR(256) NOT NULL ,
-  enabled TINYINT NOT NULL DEFAULT 1 ,
+  enabled BOOLEAN NOT NULL DEFAULT TRUE ,
   PRIMARY KEY (username));
 
 CREATE TABLE user_roles (
