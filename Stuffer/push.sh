8bb2504455d0178fb86ba9cfe7d104cc870d1a40
iwaneez <jappking@gmail.com>
2017-05-08 21:40:12 +0200

    updated docker build and run files

---
+++ Stuffer/push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker login
+docer push iwaneez/stuffer
--- Stuffer/run.sh
+++ Stuffer/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo docker build -t stuffer .
-sudo docker run --name stufferCnt stuffer -p 8080:8080
+docker build -t iwaneez/stuffer .
+docker run -e spring.profiles.active="dev" -p 8080:8080 --name stuffer iwaneez/stuffer
