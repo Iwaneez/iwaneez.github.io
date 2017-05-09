24a7197da8b38be4d5fa5c37a91954bc180491a3
Ivan Harmady <ivan.harmady@anext.sk>
2017-05-09 16:04:32 +0200

    added postgre docker support

--- Stuffer/run.sh
+++ Stuffer/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 docker build -t iwaneez/stuffer .
-docker run -e spring.profiles.active="dev" -p 8080:8080 --name stuffer iwaneez/stuffer
+
+docker run --name postgreDB -p 5432:5432 -e POSTGRES_DB=stuffer -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=root123 -d postgres:9.6.2
+docker run --name stuffer -p 8080:8080 -e spring.profiles.active=dev --link postgreDB:postgre iwaneez/stuffer
