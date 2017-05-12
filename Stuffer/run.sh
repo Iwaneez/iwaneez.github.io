f045a4f50e74824a8db13c7705641fd6a9908fbb
iwaneez <jappking@gmail.com>
2017-05-12 02:08:12 +0200

    added volume and docker-compose support

--- Stuffer/run.sh
+++ Stuffer/run.sh
@@ -2,5 +2,5 @@
 
 docker build -t iwaneez/stuffer .
 
-docker run --name postgreDB -p 5432:5432 -e POSTGRES_DB=stuffer -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=root123 -d postgres:9.6.2
+docker run --name postgreDB -d -p 5432:5432 -e POSTGRES_DB=stuffer -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=root123 -v /home/iwaneez/postgreData:/var/lib/postgresql/data postgres:9.6.2
 docker run --name stuffer -p 8080:8080 -e spring.profiles.active=dev --link postgreDB:postgre iwaneez/stuffer
