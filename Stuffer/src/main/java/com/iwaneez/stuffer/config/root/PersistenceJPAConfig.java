29092be4dd2e879c51a83855bc302ac5a76920dc
iwaneez <jappking@gmail.com>
2017-05-14 13:53:32 +0200

    JPA config refactoring, hibernate entity db autocreate now working

--- Stuffer/src/main/java/com/iwaneez/stuffer/config/root/PersistenceJPAConfig.java
+++ Stuffer/src/main/java/com/iwaneez/stuffer/config/root/PersistenceJPAConfig.java
@@ -1,35 +1,16 @@
 package com.iwaneez.stuffer.config.root;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource({"classpath:application-dev.properties"})
-@EnableJpaRepositories("com.iwaneez.stuffer.persistence")
 public class PersistenceJPAConfig {
-
-    private static final String ENTITY_PACKAGES_TO_SCAN = "com.iwaneez.stuffer.persistence";
-
-    @Autowired
-    private Environment env;
-
-    @Autowired
-    private DataSource dataSource;
 
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
@@ -37,23 +18,6 @@ public class PersistenceJPAConfig {
         transactionManager.setEntityManagerFactory(emf);// entityManagerFactory().getObject()
 
         return transactionManager;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan(new String[]{ENTITY_PACKAGES_TO_SCAN});
-
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        return em;
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
     }
 
 }
