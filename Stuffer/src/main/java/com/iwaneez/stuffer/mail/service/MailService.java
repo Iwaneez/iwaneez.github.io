d5f60588fbd1f6dd33c5beab6b2a320b84a9766c
iwaneez <wfnna9akf>
2018-03-27 00:36:52 +0200

    added java mail, thymeleaf and jasypt support

---
+++ Stuffer/src/main/java/com/iwaneez/stuffer/mail/service/MailService.java
@@ -0,0 +1,59 @@
+package com.iwaneez.stuffer.mail.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+
+import javax.mail.internet.MimeMessage;
+import java.util.Locale;
+
+@Service
+public class MailService {
+
+    private final Logger log = LoggerFactory.getLogger(MailService.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendEmail(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        log.debug("Send email[multipart '{}' and html '{}'] to '{}' with subject '{}' and content={}",
+                isMultipart, isHtml, to, subject, content);
+
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, "UTF-8");
+            message.setTo(to);
+//            message.setFrom("ivan.harmady@systemaccess.org");
+            message.setSubject(subject);
+            message.setText(content, isHtml);
+            javaMailSender.send(mimeMessage);
+            log.debug("Sent email to User '{}'", to);
+        } catch (Exception e) {
+            if (log.isDebugEnabled()) {
+                log.warn("Email could not be sent to user '{}'", to, e);
+            } else {
+                log.warn("Email could not be sent to user '{}': {}", to, e.getMessage());
+            }
+        }
+    }
+
+    @Async
+    public void sendEmailFromTemplate(String recipient, String subject, String templateName, Object mailData, Locale locale) {
+        Context context = new Context(locale);
+        context.setVariable("mailData", mailData);
+
+        String content = templateEngine.process(templateName, context);
+        sendEmail(recipient, subject, content, false, true);
+    }
+
+}
