332a510ccab13d87f7dad8a5fdbed3651c9fdbbd
iwaneez <wfnna9akf>
2018-04-13 01:37:49 +0200

    scripting to create new ssl cert every month

---
+++ docker_images/nginx/get_cert.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo docker run -it --rm \
+      -v /home/ubuntu/nginx/certs:/etc/letsencrypt \
+      -v /home/ubuntu/nginx/certs-data:/data/letsencrypt \
+      certbot/certbot \
+      certonly --webroot \
+      --register-unsafely-without-email --agree-tos \
+      --webroot-path=/data/letsencrypt \
+      -d harmady.com -d www.harmady.com \
+      -d stuffer.harmady.com \
+      -d blog.harmady.com
