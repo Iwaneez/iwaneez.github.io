0561fe78dc0b32caed52cc484ad12beb90082029
iwaneez <wfnna9akf>
2018-04-14 15:51:18 +0200

    updated domains in get_cert.sh

--- docker_images/nginx/get_cert.sh
+++ docker_images/nginx/get_cert.sh
@@ -8,5 +8,5 @@ sudo docker run -it --rm \
       --register-unsafely-without-email --agree-tos \
       --webroot-path=/data/letsencrypt \
       -d harmady.com -d www.harmady.com \
-      -d stuffer.harmady.com \
-      -d blog.harmady.com
+      -d stuffer.harmady.com -d www.stuffer.harmady.com \
+      -d blog.harmady.com -d www.blog.harmady.com
