a009c872b2a87dc8c4c9921e9dc1ee180982d89e
iwaneez <ivan.harmady@gmail.com>
2018-10-01 09:26:04 +0200

    reordered upload script commands

--- stuffer-app/push_online.sh
+++ stuffer-app/push_online.sh
@@ -4,6 +4,22 @@
 cmdtag="[STUFFER UPDATE]"
 remote="ssh -i ~/ssh/aws_pk.pem ubuntu@ec2-18-194-201-221.eu-central-1.compute.amazonaws.com"
 
+# Building *.jar file
+echo "$cmdtag Building application"
+mvn clean install
+echo "$cmdtag Application was successfully built"
+
+# Building docker image
+echo "$cmdtag Building docker image from application"
+sudo docker build -t stuffer_stuffer stuffer-app
+sudo docker tag stuffer_stuffer:latest 773539405688.dkr.ecr.eu-central-1.amazonaws.com/stuffer:latest
+echo "$cmdtag Docker image was successfully built"
+
+# Pushing image into repository
+echo "$cmdtag Pushing docker image into the repository"
+sudo docker push 773539405688.dkr.ecr.eu-central-1.amazonaws.com/stuffer:latest
+echo "$cmdtag Docker image was successfully pushed into the repository"
+
 # Getting login command to stuffer repository
 echo "$cmdtag Getting logging command to remote repository"
 command="sudo $(~/.local/bin/aws ecr get-login --no-include-email --region eu-central-1)"
@@ -14,22 +30,6 @@ echo "$command"
 echo "$cmdtag Logging into remote repository"
 eval "${command}"
 echo "$cmdtag Successfully logged into remote repository"
-
-# Building *.jar file
-echo "$cmdtag Building application"
-mvn clean install
-echo "$cmdtag Application was successfully built"
-
-# Building docker image
-echo "$cmdtag Building docker image from application"
-sudo docker build -t stuffer_stuffer Stuffer
-sudo docker tag stuffer_stuffer:latest 773539405688.dkr.ecr.eu-central-1.amazonaws.com/stuffer:latest
-echo "$cmdtag Docker image was successfully built"
-
-# Pushing image into repository
-echo "$cmdtag Pushing docker image into the repository"
-sudo docker push 773539405688.dkr.ecr.eu-central-1.amazonaws.com/stuffer:latest
-echo "$cmdtag Docker image was successfully pushed into the repository"
 
 # Updating application on server from repository
 echo "$cmdtag Running commands on remote server..."
